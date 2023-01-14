ee400096e724b11c5763b3bf5c76fca60e17f5d1
bakosrozsa <bakosrozsa01@gmail.com>
2023-01-14 17:39:54 +0100

    NextPage onclick events

--- script.js
+++ script.js
@@ -8,11 +8,57 @@ function StartPage ()
 }
 function PrevPage ()
 {
-    console.log('Vissza');
+    var x = document.getElementById('jump_page').value;
 }
 function NextPage ()
 {
-    console.log('Előre');
+    var x = document.getElementById('jump_page').value; 
+    switch(x) {
+        case 'original':
+            document.getElementById('original').style.display='none';
+            document.getElementById('prequel').style.display='flex';
+            document.getElementById('sequel').style.display='none';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('mandalorian').style.display='none';
+            document.getElementById('jump_page').value = 'prequel'
+        break;
+        case 'prequel':
+            document.getElementById('original').style.display='none';
+            document.getElementById('prequel').style.display='none';
+            document.getElementById('sequel').style.display='flex';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('mandalorian').style.display='none';
+            document.getElementById('jump_page').value = 'sequel'
+        break;
+        case 'sequel':
+            document.getElementById('original').style.display='none';
+            document.getElementById('prequel').style.display='none';
+            document.getElementById('sequel').style.display='none';
+            document.getElementById('spinoff').style.display='flex';
+            document.getElementById('mandalorian').style.display='none';
+            document.getElementById('jump_page').value = 'spinoff'
+        break;
+        case 'spinoff':
+            document.getElementById('original').style.display='none';
+            document.getElementById('prequel').style.display='none';
+            document.getElementById('sequel').style.display='none';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('mandalorian').style.display='flex';
+            document.getElementById('jump_page').value = 'mandalorian'
+        break;
+        case 'mandalorian':
+            document.getElementById('original').style.display='flex';
+            document.getElementById('prequel').style.display='none';
+            document.getElementById('sequel').style.display='none';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('mandalorian').style.display='none';
+            document.getElementById('jump_page').value = 'original'
+        break;
+        default:
+            document.getElementById('jump_page').value = 'original'
+        break;
+      }
+
 }
 function SelectedPage ()
 {
@@ -54,6 +100,7 @@ function SelectedPage ()
             document.getElementById('mandalorian').style.display='flex';
         break;
         default:
+            //document.getElementById('jump_page').value = 'original'
         break;
       }
 }
