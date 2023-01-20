c038f3f1fc16822e6ba4faa85c75371b99f7b47d
bakosrozsa <bakosrozsa01@gmail.com>
2023-01-20 19:33:19 +0100

    Got oldalon kártyák újracsoportosítása js-ben

--- GOT/script.js
+++ GOT/script.js
@@ -1,26 +1,36 @@
 function StartPage ()
 {
-    document.getElementById('I-IV').style.display='flex';
-    document.getElementById('V-VIII').style.display='none';
+    document.getElementById('I-III').style.display='flex';
+    document.getElementById('IV-VI').style.display='none';
+    document.getElementById('VII-VIII').style.display='none';
     document.getElementById('spinoff').style.display='none';
 }
 function SelectedPage ()
 {
     var x = document.getElementById('jump_page').value;
     switch(x) {
-        case 'I-IV':
-            document.getElementById('I-IV').style.display='flex';
-            document.getElementById('V-VIII').style.display='none';
+        case 'I-III':
+            document.getElementById('I-III').style.display='flex';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='none';
         break;
-        case 'V-VIII':
-            document.getElementById('I-IV').style.display='none';
-            document.getElementById('V-VIII').style.display='flex';
+        case 'IV-VI':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='flex';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='none';
+        break;
+        case 'VII-VIII':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='flex';
+            document.getElementById('spinoff').style.display='none';    
         break;
         case 'spinoff':
-            document.getElementById('I-IV').style.display='none';
-            document.getElementById('V-VIII').style.display='none';
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='flex';
         break;
         default:
@@ -33,55 +43,74 @@ function PrevPage ()
 {
     var x = document.getElementById('jump_page').value;
     switch(x) {
-        case 'I-IV':
-            document.getElementById('I-IV').style.display='none';
-            document.getElementById('V-VIII').style.display='none';
+        case 'I-III':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='flex';
             document.getElementById('jump_page').value = 'spinoff'
         break;
-        case 'V-VIII':
-            document.getElementById('I-IV').style.display='flex';
-            document.getElementById('V-VIII').style.display='none';
+        case 'IV-VI':
+            document.getElementById('I-III').style.display='flex';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='none';
-            document.getElementById('jump_page').value = 'I-IV'
+            document.getElementById('jump_page').value = 'I-III'
+        break;
+        case 'VII-VIII':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='flex';
+            document.getElementById('VII-VIII').style.display='none';
+            document.getElementById('spinoff').style.display='none';    
+            document.getElementById('jump_page').value = 'IV-VI'
         break;
         case 'spinoff':
-            document.getElementById('I-IV').style.display='none';
-            document.getElementById('V-VIII').style.display='flex';
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='flex';
             document.getElementById('spinoff').style.display='none';
-            document.getElementById('jump_page').value = 'V-VIII'
+            document.getElementById('jump_page').value = 'VII-VIII'
         break;
         default:
-            document.getElementById('jump_page').value = 'I-IV'
+            document.getElementById('jump_page').value = 'I-III'
         break;
       }
-
 }
+
 function NextPage ()
 {
-    var x = document.getElementById('jump_page').value; 
+    var x = document.getElementById('jump_page').value;
     switch(x) {
-        case 'I-IV':
-            document.getElementById('I-IV').style.display='none';
-            document.getElementById('V-VIII').style.display='flex';
+        case 'I-III':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='flex';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='none';
-            document.getElementById('jump_page').value = 'V-VIII'
+            document.getElementById('jump_page').value = 'IV-VI'
         break;
-        case 'V-VIII':
-            document.getElementById('I-IV').style.display='none';
-            document.getElementById('V-VIII').style.display='none';
-            document.getElementById('spinoff').style.display='flex';
+        case 'IV-VI':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='flex';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('jump_page').value = 'VII-VIII'
+        break;
+        case 'VII-VIII':
+            document.getElementById('I-III').style.display='none';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='none';
+            document.getElementById('spinoff').style.display='flex';    
             document.getElementById('jump_page').value = 'spinoff'
         break;
         case 'spinoff':
-            document.getElementById('I-IV').style.display='flex';
-            document.getElementById('V-VIII').style.display='none';
+            document.getElementById('I-III').style.display='flex';
+            document.getElementById('IV-VI').style.display='none';
+            document.getElementById('VII-VIII').style.display='none';
             document.getElementById('spinoff').style.display='none';
-            document.getElementById('jump_page').value = 'I-IV'
+            document.getElementById('jump_page').value = 'I-III'
         break;
         default:
-            document.getElementById('jump_page').value = 'I-IV'
+            document.getElementById('jump_page').value = 'I-III'
         break;
       }
-
 }
