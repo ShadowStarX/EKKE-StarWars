4a08650f65e382c2ea1b61a7fe525da6a4246394
bakosrozsa <bakosrozsa01@gmail.com>
2023-01-20 19:05:43 +0100

    Kártyák csoportokban megjelenítése, button esetén a got oldalon

--- GOT/script.js
+++ GOT/script.js
@@ -22,3 +22,60 @@ function SelectedPage ()
         break;
       }
 }
+
+function PrevPage ()
+{
+    var x = document.getElementById('jump_page').value;
+    switch(x) {
+        case 'I-IV':
+            document.getElementById('I-IV').style.display='none';
+            document.getElementById('V-VIII').style.display='none';
+            document.getElementById('spinoff').style.display='flex';
+            document.getElementById('jump_page').value = 'spinoff'
+        break;
+        case 'V-VIII':
+            document.getElementById('I-IV').style.display='flex';
+            document.getElementById('V-VIII').style.display='none';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('jump_page').value = 'I-IV'
+        break;
+        case 'spinoff':
+            document.getElementById('I-IV').style.display='none';
+            document.getElementById('V-VIII').style.display='flex';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('jump_page').value = 'V-VIII'
+        break;
+        default:
+            document.getElementById('jump_page').value = 'I-IV'
+        break;
+      }
+
+}
+function NextPage ()
+{
+    var x = document.getElementById('jump_page').value; 
+    switch(x) {
+        case 'I-IV':
+            document.getElementById('I-IV').style.display='none';
+            document.getElementById('V-VIII').style.display='flex';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('jump_page').value = 'V-VIII'
+        break;
+        case 'V-VIII':
+            document.getElementById('I-IV').style.display='none';
+            document.getElementById('V-VIII').style.display='none';
+            document.getElementById('spinoff').style.display='flex';
+            document.getElementById('jump_page').value = 'spinoff'
+        break;
+        case 'spinoff':
+            document.getElementById('I-IV').style.display='flex';
+            document.getElementById('V-VIII').style.display='none';
+            document.getElementById('spinoff').style.display='none';
+            document.getElementById('jump_page').value = 'I-IV'
+        break;
+        default:
+            document.getElementById('jump_page').value = 'I-IV'
+        break;
+      }
+
+}
